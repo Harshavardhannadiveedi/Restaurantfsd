709bb5f2589a364496b2a3ada0e65bbda44745ca
HARSHAVARDHAN NADIVEEDI <harshavardhannadiveedi@gmail.com>
2024-11-24 18:32:38 +0530

    first commit

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Harsha</div>
+          <div className="right">
+            <p>Madanapalli</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Harsha07</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Harsha07.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
